7d33d10817bdd61b0c87db94884a8175b3d73333
jackjackE <tmddjs210@naver.com>
2021-01-23 15:09:57 +0900

    10. Create Menu fo AddFragment

--- ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/add/AddFragment.kt
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/add/AddFragment.kt
@@ -1,9 +1,7 @@
 package com.mtjin.todoapp.fragments.add
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.mtjin.todoapp.R
 
@@ -14,7 +12,13 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //메뉴 세팅
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 }
