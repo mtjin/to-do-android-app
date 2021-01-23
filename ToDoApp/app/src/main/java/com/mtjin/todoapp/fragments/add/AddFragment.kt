6c1fea46fe362dd53930ab909aab9b1237365e09
jackjackE <tmddjs210@naver.com>
2021-01-23 13:55:47 +0900

    7. Design UpdateFragment Layout

--- ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/add/AddFragment.kt
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/add/AddFragment.kt
@@ -1,4 +1,4 @@
-package com.mtjin.todoapp.fragments
+package com.mtjin.todoapp.fragments.add
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mtjin.todoapp.R
 
-class UpdateFragment : Fragment() {
+
+class AddFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
 }
--- ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/update/UpdateFragment.kt
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/update/UpdateFragment.kt
@@ -1,4 +1,4 @@
-package com.mtjin.todoapp.fragments
+package com.mtjin.todoapp.fragments.update
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mtjin.todoapp.R
 
-
-class AddFragment : Fragment() {
+class UpdateFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_update, container, false)
     }
 }
