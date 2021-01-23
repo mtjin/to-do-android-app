1454bdcb2133bef972ebb7680cd43ec257dbcaf5
jackjackE <tmddjs210@naver.com>
2021-01-23 12:56:43 +0900

    5. Create Navigation Graph

---
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/fragments/AddFragment.kt
@@ -0,0 +1,20 @@
+package com.mtjin.todoapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mtjin.todoapp.R
+
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+}
