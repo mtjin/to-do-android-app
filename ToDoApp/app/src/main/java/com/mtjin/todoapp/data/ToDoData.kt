780d238e68cde4c929af051375dac9aac1ca9db7
jackjackE <tmddjs210@naver.com>
2021-01-24 20:02:35 +0900

    16. Create Dao (ToDoDao)

---
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/data/ToDoData.kt
@@ -0,0 +1,12 @@
+package com.mtjin.todoapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    var title: String,
+    var priority: Priority,
+    var descreiption: String
+)
