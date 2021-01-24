f9faca54fb04e20a4427bfc49f09bb546f18a03a
jackjackE <tmddjs210@naver.com>
2021-01-24 20:49:19 +0900

    18. Create Database (ToDoDatabase)

---
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/data/ToDoDatabase.kt
@@ -0,0 +1,36 @@
+package com.mtjin.todoapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+
+        //Multi Thread 환경에서 하나의 Thread만(Main Memory) read & write하고 나머지 Thread가 read하는 상황에서 가장 최신의 값을 보장합니다.
+        //https://nesoy.github.io/articles/2018-06/Java-volatile
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getDatabase(context: Context): ToDoDatabase {
+            val tempInstance = INSTANCE
+            //싱글톤
+            if (tempInstance != null) return tempInstance
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ToDoDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
