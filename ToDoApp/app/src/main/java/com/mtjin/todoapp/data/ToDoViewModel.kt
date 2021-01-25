f8acc2cee4ff4a77964573e194e5035532a15387
jackjackE <tmddjs210@naver.com>
2021-01-25 21:30:52 +0900

    21. Create ViewModel (ToDoViewModel)

---
+++ ToDoApp/app/src/main/java/com/mtjin/todoapp/data/ToDoViewModel.kt
@@ -0,0 +1,29 @@
+package com.mtjin.todoapp.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.mtjin.todoapp.data.models.ToDoData
+import com.mtjin.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
